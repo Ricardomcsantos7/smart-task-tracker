f6f34d12a1dfeb254701948e0d7a489ead3745ba
Ricardo Santos <ricardomcsantos7@gmail.com>
2026-02-27 11:23:33 +0100

    Initial Vite setup and basic App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  return (
+    <div>
+      <h1>Smart Task Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
