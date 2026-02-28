ae999b1902e44789ae1fb6c852beb8fd1c335710
Ricardo Santos <ricardomcsantos7@gmail.com>
2026-02-28 22:16:38 +0100

    Add Task List display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -18,6 +19,7 @@ function App() {
     <div>
       <h1>Smart Task Tracker</h1>
       <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
